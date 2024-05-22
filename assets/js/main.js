36406b4f85370fee172d5a0faea2d9c3f94985d7
nightfury-305244 <jonathanm33952@gmail.com>
2024-05-22 17:34:58 -0400

    fix proper index

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 function scrollHeader(){
     const header = document.getElementById('header')
-    if(this.scrollY >= 20) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+    if(this.scrollY >= 10) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
